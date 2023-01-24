7eaa8cbf9e4c0ffe65c0aacd8b06e3b1ca809645
SLM <sabrina.martorelli@gmail.com>
2023-01-24 09:44:37 +0000

    Adds starter code for inquirer prompt

--- index.js
+++ index.js
@@ -15,6 +15,45 @@ function writeToFile(fileName, data) {
 // function to initialize program
 function init() {
 
+
+    inquirer.prompt([
+        {
+          //  type: 'input',
+            name: 'name',
+            message : 'Please enter your name:',
+        },
+        {
+          //  type: 'input',
+            name: 'age',
+            message : 'Please enter your age:',
+        },
+        {
+            type: 'list',
+              name: 'option',
+              message : 'Please enter your type choice:',
+              choices: ['one', 'two', 'three'],
+          },
+        {
+          //  type: 'input',
+            name: 'type',
+            message : 'Please enter the command :',
+        }
+    
+      ])
+    
+    
+    .then(data => {
+        if (true) {  //LOOK AT THIS
+          
+            generateMarkdown();
+    
+        }
+    console.log('Thanks for using our api');
+    
+      });
+    }
+
+
 }
 
 // function call to initialize program
